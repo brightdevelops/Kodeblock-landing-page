b1a0830272ef108e1e601df03ed5354cdc794d4d
Oluwatosin <tosindaniel360@gmail.com>
2025-10-23 13:15:33 +0100

    refactor: simplify Home component structure and clean up unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,24 @@
-// src/pages/Home.jsx
-
-import projects from '../sections/About.jsx';
-import CTA from '../sections/Project.jsx';
+import React from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+import Project from '../sections/Project.jsx';
 import Hero from '../sections/Hero.jsx';
 import Team from '../sections/Team.jsx';
 import About from '../sections/About.jsx';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>KODEBLOCK Landing Page</h1>
-      <p>If you see this, routing/imports are correct.</p>
+    <div className="min-h-screen bg-[#ebe2d6] text-[#1e1e1e]">
+      {/* <Navbar />
+      <main className="relative z-10">
+        <Hero />
+        <Project />
+        <About />
+        <Team />
+      </main>
+      <Footer /> */}
     </div>
-  );
+  )
 }
 
-// export default function Home() {
-//   return (
-//     <div className="min-h-screen bg-[#ebe2d6] text-[#1e1e1e]">
-//       <Navbar />
-//       <main className="relative z-10">
-//         <Hero />
-//         <Projects />
-//         <About />
-//         <Team />
-//         <CTA />
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
+export default Home
