c927f6136588df33f2fa5e720b0ce531c18156fc
Oluwatosin <tosindaniel360@gmail.com>
2025-11-03 08:47:40 +0100

    styled the scrollbar and updaed the renderings in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero'
 import Project from './components/Project'
 import About from './components/About'
 import Team from './components/Team'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
         <Project />
         <About />
         <Team />
+        <CTA />
+        <Footer />
       </main>
     </>
   )
